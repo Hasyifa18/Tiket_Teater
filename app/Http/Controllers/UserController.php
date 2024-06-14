5048252782d7e55d7f283b9ba8424ced341a262f
salymah <salymahallawiyah998@gmail.com>
2024-06-14 18:08:01 +0700

    bagian fitur delete-user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\models\fligh;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function update($id){
+        $user = User::find($id);
+        $user->update();
+        return redirect()->route('user.index');
+    }
 }
