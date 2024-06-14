9ca55b82f2a9f9152192e210d015a3f9c254e9f0
windanengsi <windanengsi5769@gmail.com>
2024-06-14 16:54:16 +0700

    mengerjakan fitur delete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function destroy($id){
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->route('user.index');
+    }
 }
