f50ede129362c85fc99f4e7a0ef0e02e7a82217a
salymah <salymahallawiyah998@gmail.com>
2024-06-14 18:29:41 +0700

    bagian fitur update-user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,14 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\models\fligh;
+
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function update($id){
-        $user = User::find($id);
-        $user->update();
-        return redirect()->route('user.index');
-    }
+    
 }
