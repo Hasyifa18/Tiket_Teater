4d7ce6d0131ab922397f11490944246c7a6b8a8d
salymah <salymahallawiyah998@gmail.com>
2024-06-21 16:59:05 +0700

    penambahan fitur update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\flight;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+   public function edit($id){
+    dd($id);
+   }
+
+   public function update(Request $request){
+   }
 }
